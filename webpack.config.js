bbe2a8a1b5d2438d06d840c820a4706f215cdb97
Renata Jachtoma Mendak <prana861@gmail.com>
2018-05-31 17:03:03 +0200

    wersja 1.0

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require("path");
+
+module.exports = {
+    entry: "./js/app.js",
+    output: {
+        filename: "js/out.js",
+        path: path.resolve(__dirname, ".")
+    },
+    watch: false,
+    mode : 'production',
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, '.'),
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [["env", {
+                            targets: {
+                                browsers: [
+                                    '> 1%', 'last 2 versions'
+                                ]
+                            }
+                        }]]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: "raw-loader"
+            }
+        ]
+    }
+}
